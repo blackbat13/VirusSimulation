03617b233ebf4f3ff7caa5b06fcd1be1f24cf46a
blackbat <blackbat13@gmail.com>
2020-05-01 22:09:30 +0200

    Changed color definitions.

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 from human_status import HumanStatus
 
-BG_COLOR = "#fff"
+BG_COLOR = "#ffffff"
 
 HUMAN_SIZE_RADIUS = 5
 HUMAN_CHANGE_VELOCITY_PROBABILITY = 0.05
 HUMAN_MAX_VELOCITY = 2
 
-HUMAN_STATUS_COLOR = {HumanStatus.SICK: "#f00",
-                      HumanStatus.HEALTHY: "#0f0",
+HUMAN_STATUS_COLOR = {HumanStatus.SICK: "#ff0000",
+                      HumanStatus.HEALTHY: "#00ff00",
                       HumanStatus.CONTAGIOUS: "#fa886b",
                       HumanStatus.RECOVERED: "#e5f7b2"}
 
