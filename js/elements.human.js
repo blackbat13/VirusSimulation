04a5bddb06407a3bc765727cd05978d8bacc3957
blackbat13 <blackbat13@gmail.com>
2020-05-28 15:31:02 +0200

    Recompiled JS.

--- js/elements.human.js
+++ js/elements.human.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-05-27 12:20:35
+// Transcrypt'ed from Python, 2020-05-28 15:30:09
 var math = {};
 var random = {};
 var time = {};
--- js/org.transcrypt.__runtime__.js
+++ js/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-05-27 12:20:34
+// Transcrypt'ed from Python, 2020-05-28 15:30:08
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
